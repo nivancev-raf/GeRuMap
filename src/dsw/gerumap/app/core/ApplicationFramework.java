7ded167acdd84f7ed5a6a11da197df8bc80c0f1b
nivancev-raf <nivancev5321rn@raf.rs>
2022-10-22 15:27:20 +0200

    Kostur geru-mape

---
+++ src/dsw/gerumap/app/core/ApplicationFramework.java
@@ -0,0 +1,19 @@
+package dsw.gerumap.app.core;
+
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@NoArgsConstructor
+
+public abstract class ApplicationFramework {
+
+    protected Gui gui;
+
+    public abstract void run();
+
+    public void initialise(Gui gui){
+        this.gui = gui;
+
+    }
+}
